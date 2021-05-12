4703d78ba470b0e3c1e1230ddbc3885e69655392
shen-ui <peterhuiwang1995@gmail.com>
2021-05-12 13:31:01 -0400

    Strange behavior on gh-pages. Restablishing and fixing problems

--- src/UserList.js
+++ src/UserList.js
@@ -1,58 +1,25 @@
-import React, { useState, useRef, useCallback } from 'react';
+import React, { useState } from 'react';
 import userSearch from './userSearch.js';
 
-import './UserList.css'
-import User from './User'
+//import User from './User'
 
 export default function UserList(){
-    const [query, setQuery] = useState('')
-    const [pageNumber, setPageNumber] = useState(0)
-    const {
-        users,
-        hasMore,
-        loading,
-        error
-    } = userSearch(query, pageNumber)
-    const observer = useRef();
-    const lastUserElementRef = useCallback(node => {
-        if(loading) return
-        if(observer.current) observer.current.disconnect()
-        observer.current = new IntersectionObserver(entries => {
-            if (entries[0].isIntersecting && hasMore){
-                setPageNumber(prevPageNumber => prevPageNumber + 30)// this 30 needs to be changed to indiv.id
-            }
-        })
-
-
-        if(node) observer.current.observe(node);
-    },[loading, hasMore])
+    const [query, setQuery] = useState('');
+    const [pageNumber, setPageNumber] = useState(1);
+    
+    userSearch(query, pageNumber);
 
     function handleSearch (e){
-        setQuery(e.target.value)
+        setQuery(e.target.value);
         setPageNumber(1)
     }
 
     return (
         <>
-            <input className="search-bar" value={query} placeholder="Search GitHub" type='text' onChange={handleSearch}></input>
-            {users.map(user =>{
-                return user.map((indiv,index) => {
-                    if (user.length === index + 1){
-                                // if the user is equal to the last index, call the ref function
-                                //console.log(index)
-                        return <User key={index} link={indiv.url} index={index} ref={lastUserElementRef}/>
-                    }
-                    else{
-                                //  else, generate a normal user div
-                                //  console.log(index)
-                        return <User key={index} link={indiv.url} index={index}/>
-                    }
-                          
-                    
-                })
-            })}
-            <div className='loading'>{loading && 'Loading...'}</div>
-            <div className='error'>{error && 'Error'}</div>
+            <input type='text' onChange={handleSearch}></input>
+            <div>title</div>
+            <div>loading..</div>
+            <div>error</div>
         </>
     )
 }
--- src/userSearch.js
+++ src/userSearch.js
@@ -1,46 +1,32 @@
 import { useEffect, useState } from  'react'
 import axios from 'axios'
 
-
 export default function useBookSearch(query, pageNumber){
     const [loading, setLoading] = useState(true)
     const [error, setError] = useState(false)
     const [users, setUsers] = useState([])
-    const [ hasMore, setHasMore ] = useState(false)
+    const [ hasMOre, setHasMore ] = useState(false)
 
     useEffect(() => {
-        if (query !== ''){
-            //getUserSearch();
-        }
-
         setLoading(true)
         setError(false)
 
         let cancel
-
-        //axios for api call
         axios({
             method: 'GET',
             url: `https://api.github.com/users`,
-            params: {since: pageNumber},
+            params: {login: query, page: pageNumber},
             cancelToken: new axios.CancelToken(c => cancel = c)
         }).then(res => {
             setUsers(prevUsers =>{
-                // for some reason, set is not removing duplicates. 
-                // May have something to do with the way the dom renders.
-                var uniqueSet = [...new Set([...prevUsers, res.data.map(users => users)])]
-                return uniqueSet
+                return new Set([...prevUsers, res.data])
             })
-            setHasMore(res.data.length > 0)
-            setLoading(false)
-           //console.log(res.data.map(users =>  users))
+            console.log(res.data)
         }).catch(e => {
             if (axios.isCancel(e)) return
-            alert(e);
-            setError(true)
         })
         return() => cancel()
     }, [query, pageNumber])
 
-    return {loading, error, users, hasMore}
+    return
 }
